b1d9351397cbda9a63a8c34b7fd1dcdf0d1969b0
Leo Dion <leogdion@brightdigit.com>
2018-02-07 13:37:48 -0500

    Initial Commit

---
+++ ParallelFlock-iOS/ParallelFlock_iOS.h
@@ -0,0 +1,19 @@
+//
+//  ParallelFlock_iOS.h
+//  ParallelFlock-iOS
+//
+//  Created by Leo Dion on 2/7/18.
+//  Copyright © 2018 Bright Digit, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ParallelFlock_iOS.
+FOUNDATION_EXPORT double ParallelFlock_iOSVersionNumber;
+
+//! Project version string for ParallelFlock_iOS.
+FOUNDATION_EXPORT const unsigned char ParallelFlock_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ParallelFlock_iOS/PublicHeader.h>
+
+
