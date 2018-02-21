698e3822b2a2d585d8e74f5c0e97f050f86dfda0
Leo Dion <leogdion@brightdigit.com>
2018-02-21 08:10:41 -0500

    fixed ios versions and tests

--- ParallelFlock/Parallel.swift
+++ ParallelFlock/Parallel.swift
@@ -14,20 +14,20 @@ public extension Array {
   }
 }
 
-extension Parallel {
-  func async(_ closure: @escaping (T, T) -> T) -> ParallelReduceItemClosure<T> {
+public extension Parallel {
+  public func async(_ closure: @escaping (T, T) -> T) -> ParallelReduceItemClosure<T> {
     return {
       $2(closure($0, $1))
     }
   }
 
-  func async<U>(_ closure: @escaping (T) -> U) -> ParallelMapItemClosure<T, U> {
+  public func async<U>(_ closure: @escaping (T) -> U) -> ParallelMapItemClosure<T, U> {
     return {
       $1(closure($0))
     }
   }
 
-  func map<U>(
+  public func map<U>(
     _ each: @escaping ParallelMapItemClosure<T, U>,
     completion: @escaping ParallelMapCompletionClosure<U>) -> ParallelMapOperation<T, U> {
     let operation = ParallelMapOperation(source: self.source, itemClosure: each, completion: completion)
@@ -35,13 +35,13 @@ extension Parallel {
     return operation
   }
 
-  func map<U>(
+  public func map<U>(
     _ each: @escaping (T) -> U,
     completion: @escaping ParallelMapCompletionClosure<U>) -> ParallelMapOperation<T, U> {
     return self.map(self.async(each), completion: completion)
   }
 
-  func reduce(
+  public func reduce(
     _ each: @escaping ParallelReduceItemClosure<T>,
     completion: @escaping ParallelCompletionClosure<T>) -> ParallelReduceOperation<T> {
     let operation = ParallelReduceOperation(source: self.source, itemClosure: each, completion: completion)
@@ -49,7 +49,7 @@ extension Parallel {
     return operation
   }
 
-  func reduce(
+  public func reduce(
     _ each: @escaping (T, T) -> T,
     completion: @escaping ParallelCompletionClosure<T>) -> ParallelReduceOperation<T> {
     return self.reduce(self.async(each), completion: completion)
