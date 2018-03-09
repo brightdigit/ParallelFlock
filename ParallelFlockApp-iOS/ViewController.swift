a28d9c771a5ad2f7c0bc2d3dcdcdaecc5c4626d9
Leo Dion <leogdion@brightdigit.com>
2018-03-09 13:51:15 -0500

    adding timer

--- ParallelFlockApp-iOS/ViewController.swift
+++ ParallelFlockApp-iOS/ViewController.swift
@@ -5,26 +5,46 @@ class ViewController: UIViewController {
   @IBOutlet var completedLabel: UILabel!
   @IBOutlet var runningLabel: UILabel!
   @IBOutlet var activityIndicatorView: UIActivityIndicatorView!
-  var operation: ParallelMapOperation<Void, Void>?
+  var startTime: Date?
+  var endTime: Date?
+
+  #if USE_PARALLEL
+    var operation: ParallelMapOperation<Void, Void>?
+  #endif
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
-
-    let source = [Void].init(repeating: Void(), count: 100_000)
-    self.operation = source.parallel.map({
-      $1(Void())
-    }, completion: self.onOperationCompletion)
+    let source = [Void].init(repeating: Void(), count: 10000)
+    self.startTime = Date()
+    #if USE_PARALLEL
+      self.operation = source.parallel.map({
+        $1(Void())
+      }, completion: self.onOperationCompletion)
+    #else
+      _ = source.map { Void() }
+      self.endActivity()
+    #endif
   }
 
-  func onOperationCompletion(_: [Void]) {
-    self.operation = nil
-    DispatchQueue.main.async {
-      self.activityIndicatorView.stopAnimating()
-      self.completedLabel.isHidden = false
-      self.runningLabel.isHidden = true
+  func endActivity() {
+    self.endTime = Date()
+    self.activityIndicatorView.stopAnimating()
+    self.completedLabel.isHidden = false
+    self.runningLabel.isHidden = true
+    if let startTime = self.startTime, let endTime = self.endTime {
+      let timeInterval = endTime.timeIntervalSince(startTime)
+      self.completedLabel.text = "Completed in \(timeInterval)"
     }
   }
+
+  #if USE_PARALLEL
+    func onOperationCompletion(_: [Void]) {
+      self.operation = nil
+      self.endTime = Date()
+      DispatchQueue.main.async(execute: self.endActivity)
+    }
+  #endif
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
