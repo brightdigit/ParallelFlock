b1ca5aaa969bfb214b1401a12a4276888946c4c4
Leo Dion <leogdion@brightdigit.com>
2018-03-09 09:14:38 -0500

    working on performance testing

--- ParallelFlockApp-iOS/ViewController.swift
+++ ParallelFlockApp-iOS/ViewController.swift
@@ -1,9 +1,29 @@
+import ParallelFlock
 import UIKit
 
 class ViewController: UIViewController {
+  @IBOutlet var completedLabel: UILabel!
+  @IBOutlet var runningLabel: UILabel!
+  @IBOutlet var activityIndicatorView: UIActivityIndicatorView!
+  var operation: ParallelMapOperation<Void, Void>?
+
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+
+    let source = [Void].init(repeating: Void(), count: 100_000)
+    self.operation = source.parallel.map({
+      $1(Void())
+    }, completion: self.onOperationCompletion)
+  }
+
+  func onOperationCompletion(_: [Void]) {
+    self.operation = nil
+    DispatchQueue.main.async {
+      self.activityIndicatorView.stopAnimating()
+      self.completedLabel.isHidden = false
+      self.runningLabel.isHidden = true
+    }
   }
 
   override func didReceiveMemoryWarning() {
